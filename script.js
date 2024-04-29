32c987d377f08b4efade99a63c123a6556fca9ed
junior <umjuniorgomes@gmail.com>
2024-04-29 13:29:38 -0300

    Programinha em homenagem ao comando cowsay

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+  function showCowsay(text) {
+   
+    var cowsayText = `
+        <pre>
+ <  ${text} >
+---------------
+ \\
+  \\
+   \\          __---__
+           _-       /--______
+      __--( /     \\ )XXXXXXXXXXX\\v.
+    .-XXX(   O   O   )XXXXXXXXXXXXXXX-
+   /XXX(       U       )        XXXXXXX\\
+ /XXXXX(                )--_  XXXXXXXXXXX\\
+/XXXXX/ (      O       )   XXXXXX   \\XXXXX\\
+XXXXX/   /            XXXXXX   \\__ \\XXXXX
+XXXXXX__/          XXXXXX         \\__---->
+---___  XXX__/          XXXXXX      \\__         /
+\\-  --__/   ___/\\  XXXXXX            /  ___--/=
+\\-\\    ___/    XXXXXX              '--- XXXXXX
+\\-\\/XXX\\ XXXXXX                      /XXXXX
+\\XXXXXXXXX   \\                    /XXXXX/
+ \\XXXXXX      >                 _/XXXXX/
+   \\XXXXX--__/              __-- XXXX/
+    -XXXXXXXX---------------  XXXXXX-
+       \\XXXXXXXXXXXXXXXXXXXXXXXXXX/
+         ""VXXXXXXXXXXXXXXXXXXV""`;
+        
+   
+    document.getElementById('cowsay-box').innerHTML = cowsayText;
+}
+
+
+var usrText = prompt("Digite algo:");
+showCowsay(usrText);
